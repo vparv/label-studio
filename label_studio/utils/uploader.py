0b4fba24a852bdf5f9205fa11a6ccc51230f8d03
Vineet Parvathala <vparv@umich.edu>
2020-06-08 18:38:49 -0400

    reverted upload method change

--- label_studio/utils/uploader.py
+++ label_studio/utils/uploader.py
@@ -34,9 +34,7 @@ def tasks_from_file(filename, file):
             tasks = [{'data': task} for task in tasks]
         elif filename.endswith('.txt'):
             lines = file.read().splitlines()
-            for line in lines:
-                tasks = {'data': {settings.UPLOAD_DATA_UNDEFINED_NAME: line.decode('utf-8')}}
-                tasks = {'data': {settings.UPLOAD_DATA_UNDEFINED_NAME: line.decode('utf-8')}}
+            tasks = [{'data': {settings.UPLOAD_DATA_UNDEFINED_NAME: line.decode('utf-8')}} for line in lines]
         elif filename.endswith('.json'):
             tasks = json.load(file)  # try simple json
         else:
