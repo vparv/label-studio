58a0c24ee2b012b4d199607a2f84b8ec9f93ed58
Vineet Parvathala <vparv@umich.edu>
2020-06-09 02:20:52 -0400

    fixed syntax issue

--- label_studio/server.py
+++ label_studio/server.py
@@ -724,7 +724,7 @@ def api_import():
         for i in range(0, len(temp)-1):
             for j in range(0, numcomps-1):
                 #print(startingindex)
-                if (startingindex < len(temp))
+                if (startingindex < len(temp)):
                     tasks.append(temp[j+startingindex])
             #tasks.append(temp[i])
             #tasks.append(temp[i])
