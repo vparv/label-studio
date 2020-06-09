7ac9080737a37fb008373d07441f79a61bc8120e
Vineet Parvathala <vparv@umich.edu>
2020-06-09 02:11:00 -0400

    update variable name

--- label_studio/server.py
+++ label_studio/server.py
@@ -721,7 +721,7 @@ def api_import():
         numcomps = 3
         startingindex = 0
         for i in range(0, len(temp)-1):
-            for j in range(0, numscomps - 1):
+            for j in range(0, numcomps - 1):
                 tasks.append(temp[j+startingindex])
             #tasks.append(temp[i])
             #tasks.append(temp[i])
