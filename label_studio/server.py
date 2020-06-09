8892c695511a6b4fcf4e6dd956bf9b334dbe4ee8
Vineet Parvathala <vparv@umich.edu>
2020-06-08 22:30:39 -0400

    revert insert tasks change

--- label_studio/server.py
+++ label_studio/server.py
@@ -710,8 +710,6 @@ def api_import():
         old_tasks = json.load(open(path))
         assert isinstance(old_tasks, list), 'Tasks from input_path must be list'
         tasks = old_tasks + new_tasks
-        
-        tasks.insert(old_tasks, len(tasks))
         logger.error("It's recommended to use directory as input_path: " +
                      project.config['input_path'] + ' -> ' + os.path.dirname(project.config['input_path']))
 
