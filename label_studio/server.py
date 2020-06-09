bde470169da9fbe15be5062bb0be37d1683c91b3
Vineet Parvathala <vparv@umich.edu>
2020-06-09 01:25:43 -0400

    update task arrangement

--- label_studio/server.py
+++ label_studio/server.py
@@ -716,7 +716,7 @@ def api_import():
         print(temp[2])
         print(temp[len(temp)-1])
         tasks.clear
-        tasks[:] = []
+        tasks[1:] = []
         for i in range(0, len(temp)-1):
             tasks.append(temp[i])
             tasks.append(temp[i])
